b427a917fddfcef08ba8745849c973eeb2ca5c56
@Fakhrutdinov <r.fakhrutdinov@galament.com>
2020-01-23 23:20:50 +0400

    create WordPair class

--- lib/src/word_pair.dart
+++ lib/src/word_pair.dart
@@ -1,1 +1,20 @@
+/// Representation of a combination of 2 words, [first] and [second].
 
+class WordPair {
+  /// The first part of the pair.
+  final String first;
+
+  /// The second part of the pair.
+  final String second;
+
+  WordPair(this.first, this.second) {
+    if (first == null || second == null) {
+      throw ArgumentError("Words of WordPair cannot be null. "
+          "Received: '$first', '$second'");
+    }
+    if (first.isEmpty || second.isEmpty) {
+      throw ArgumentError("Words of WordPair cannot be empty. "
+          "Received: '$first', '$second'");
+    }
+  }
+}
