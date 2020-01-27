73a7947b9eee26fb205a33209a6b5d69774d6699
@Fakhrutdinov <r.fakhrutdinov@galament.com>
2020-01-27 21:04:42 +0400

    prepare generateWordPairs function

--- lib/src/word_pair.dart
+++ lib/src/word_pair.dart
@@ -1,4 +1,38 @@
+import 'dart:math';
+
 /// Representation of a combination of 2 words, [first] and [second].
+
+final _random = Random();
+
+enum TypeOfPair {
+  adjective_adjective,
+  noun_noun,
+  verb_verb,
+  adjective_noun,
+  verb_adjective,
+  verb_noun,
+  random_pair
+}
+
+Iterable<WordPair> generateWordPairs(
+    {TypeOfPair typeOfPair = TypeOfPair.random_pair, Random random}) sync* {
+  random ??= _random;
+
+  String pickRandom(List<String> list) => list[random.nextInt(list.length)];
+
+  // We're in a sync* function, so `while (true)` is okay.
+  // ignore: literal_only_boolean_expressions
+  while (true) {
+    String firstWord;
+    String secondWord;
+
+    ///todo creating pairs depending on TypeOfPair
+
+    final WordPair wordPair = WordPair(firstWord, secondWord);
+
+    yield wordPair;
+  }
+}
 
 class WordPair {
   /// The first part of the pair.
