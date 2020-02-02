466426a0ed7e5a664d9c6cecde67fdf87c849ce3
@Fakhrutdinov <r.fakhrutdinov@galament.com>
2020-02-02 14:42:21 +0400

    add writing styles

--- lib/src/word_pair.dart
+++ lib/src/word_pair.dart
@@ -75,6 +75,18 @@ class WordPair {
   /// The second part of the pair.
   final String second;
 
+  String _asPascalCase;
+
+  String _asSnakeCase;
+
+  String _asCamelCase;
+
+  String _asLowerCase;
+
+  String _asUpperCase;
+
+  String _asString;
+
   WordPair(this.first, this.second) {
     if (first == null || second == null) {
       throw ArgumentError("Words of WordPair cannot be null. "
@@ -93,5 +105,64 @@ class WordPair {
         generateWordPairs(typeOfPair: typeOfPair, random: random);
     return pairsIterable.first;
   }
-  //test
+
+  /// Returns the word pair as a simple string, with second word capitalized,
+  /// like `"keyFrame"` or `"franceLand"`. This is informally called
+  /// "camel case".
+  String get asCamelCase => _asCamelCase ??= _createCamelCase();
+
+  /// Returns the word pair as a simple string, in lower case,
+  /// like `"keyframe"` or `"franceland"`.
+  String get asLowerCase => _asLowerCase ??= asString.toLowerCase();
+
+  /// Returns the word pair as a simple string, with each word capitalized,
+  /// like `"KeyFrame"` or `"BigUsa"`. This is informally called "pascal case".
+  String get asPascalCase => _asPascalCase ??= _createPascalCase();
+
+  /// Returns the word pair as a simple string, separated by an underscore,
+  /// like `"key_frame"` or `"big_usa"`. This is informally called "snake case".
+  String get asSnakeCase => _asSnakeCase ??= _createSnakeCase();
+
+  /// Returns the word pair as a simple string, like `"keyframe"`
+  /// or `"bigFrance"`.
+  String get asString => _asString ??= '$first$second';
+
+  /// Returns the word pair as a simple string, in upper case,
+  /// like `"KEYFRAME"` or `"FRANCELAND"`.
+  String get asUpperCase => _asUpperCase ??= asString.toUpperCase();
+
+  @override
+  int get hashCode =>
+      (first.hashCode.toString() + second.hashCode.toString()).hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    if (other is WordPair) {
+      return first == other.first && second == other.second;
+    } else {
+      return false;
+    }
+  }
+
+  /// Returns a string representation of the [WordPair] where the two parts
+  /// are joined by [separator].
+  ///
+  /// For example, `new WordPair('mine', 'craft').join()` returns `minecraft`.
+  String join([String separator = '']) => '$first$separator$second';
+
+  /// Creates a new [WordPair] with both parts in lower case.
+  WordPair toLowerCase() => WordPair(first.toLowerCase(), second.toLowerCase());
+
+  @override
+  String toString() => asString;
+
+  String _capitalize(String word) {
+    return "${word[0].toUpperCase()}${word.substring(1).toLowerCase()}";
+  }
+
+  String _createCamelCase() => "${first.toLowerCase()}${_capitalize(second)}";
+
+  String _createPascalCase() => "${_capitalize(first)}${_capitalize(second)}";
+
+  String _createSnakeCase() => "${first}_${second}";
 }
