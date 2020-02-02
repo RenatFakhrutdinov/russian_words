61cc693da3b0cc73d08baad8f887bf0c7e24ebbd
@Fakhrutdinov <r.fakhrutdinov@galament.com>
2020-02-02 17:05:05 +0400

    add example of getting single word

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:russian_words/src/words/all_words.dart';
+import 'package:russian_words/russian_words.dart';
+
+enum Category { any, adjective, noun, verb }
 
 void main() => runApp(MyApp());
 
@@ -8,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'russian_words demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Russian Words'),
     );
   }
 }
@@ -26,11 +30,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  List<Widget> _widgetOptions = [
+    SingleWord(),
+    Text('2'),
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
@@ -38,28 +47,138 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.looks_one), title: Text('Single word')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.looks_two), title: Text("Random pair"))
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+    );
+  }
+}
+
+class SingleWord extends StatefulWidget {
+  @override
+  _SingleWordState createState() => _SingleWordState();
+}
+
+class _SingleWordState extends State<SingleWord> {
+  Category _category = Category.any;
+  String _word;
+
+  @override
+  void initState() {
+    super.initState();
+    _word = _randomWord();
+  }
+
+  String _randomWord() {
+    switch (_category) {
+      case Category.any:
+        return allWords[Random().nextInt(allWords.length)];
+        break;
+      case Category.adjective:
+        return adjectives[Random().nextInt(adjectives.length)];
+        break;
+      case Category.noun:
+        return nouns[Random().nextInt(nouns.length)];
+        break;
+      case Category.verb:
+        return verbs[Random().nextInt(verbs.length)];
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        Text(
+          "Random single word is",
+          textAlign: TextAlign.center,
+        ),
+        Container(
+          padding: EdgeInsets.all(8),
+          decoration:
+              BoxDecoration(border: Border.all(), color: Colors.blue.shade50),
+          width: MediaQuery.of(context).size.width,
+          child: Text(
+            _word,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Column(
           children: <Widget>[
-            Text(allWords[0]),
-            Text(
-              'You have pushed the button this many times:',
+            Text('Choose category'),
+            Row(
+              children: <Widget>[
+                Radio(
+                  value: Category.any,
+                  groupValue: _category,
+                  onChanged: (category) => setState(() {
+                    _category = category;
+                    _word = _randomWord();
+                  }),
+                ),
+                Text('any'),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Row(
+              children: <Widget>[
+                Radio(
+                  value: Category.adjective,
+                  groupValue: _category,
+                  onChanged: (category) => setState(() {
+                    _category = category;
+                    _word = _randomWord();
+                  }),
+                ),
+                Text('adjective')
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Radio(
+                  value: Category.noun,
+                  groupValue: _category,
+                  onChanged: (category) => setState(() {
+                    _category = category;
+                    _word = _randomWord();
+                  }),
+                ),
+                Text('noun')
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Radio(
+                  value: Category.verb,
+                  groupValue: _category,
+                  onChanged: (category) => setState(() {
+                    _category = category;
+                    _word = _randomWord();
+                  }),
+                ),
+                Text('verb')
+              ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        RaisedButton(
+          child: Text("change word"),
+          onPressed: () => setState(() => _word = _randomWord()),
+        )
+      ],
     );
   }
 }
